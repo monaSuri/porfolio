bb88da28a11d4341e2ef1e11330014f5435cf0e2
Monali Suriarachchi <monalisuriarachchi@gmail.com>
2025-04-18 20:56:06 +0530

    completed the navbar
    added menu bar and close button.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import { assets } from '@/assets /assets'
 import Image from 'next/image'
-import React, { use } from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = () => {
 
   const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = 'translateX(-16rem)'
+  }
+
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = 'translateX(16rem)'
+  }
+
   return (
     <>
       <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex justify-between items-center z-50'>
@@ -77,33 +86,34 @@ const Navbar = () => {
             />
           </a>
 
-          <button className='block md:hidden ml-3'>
+          <button className='block md:hidden ml-3' onClick={openMenu}>
             <Image src={assets.menu_black} alt=" " className='w-6' />
           </button>
 
         </div>
 
        {/* === Mobile Menu === */}
-        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-0 
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 
         top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
 
-          <div className='absolute right-6 top-6'> 
+          <div className='absolute right-6 top-6' onClick={closeMenu}> 
             <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
           </div>
+
           <li className='md:hidden'>
-            <a className='font-Ovo' href="#top">Home</a>
+            <a className='font-Ovo' onClick={closeMenu} href="#top">Home</a>
           </li>
           <li className='md:hidden'>
-            <a className='font-Ovo' href="#about">About Me</a>
+            <a className='font-Ovo' onClick={closeMenu} href="#about">About Me</a>
           </li>
           <li className='md:hidden'>
-            <a className='font-Outfit' href="#services">Services</a>
+            <a className='font-Outfit' onClick={closeMenu} href="#services">Services</a>
           </li>
           <li className='md:hidden'>
-            <a className='font-Outfit' href="#projects">My Projects</a>
+            <a className='font-Outfit' onClick={closeMenu} href="#projects">My Projects</a>
           </li>
           <li className='md:hidden'>
-            <a className='font-Outfit' href="#contact">Contact Me</a>
+            <a className='font-Outfit' onClick={closeMenu} href="#contact">Contact Me</a>
           </li>
         </ul>
       </nav>
