5f07b31b289231e530defd7d80c5676124985aa2
Monali Suriarachchi <monalisuriarachchi@gmail.com>
2025-04-18 20:18:09 +0530

    updated navbar.tsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { assets } from '@/assets /assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { use } from 'react'
 
 const Navbar = () => {
+
+  const sideMenuRef = useRef();
   return (
     <>
       <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex justify-between items-center z-50'>
-        
+
         {/* === Social Icons (GitHub, LinkedIn, Instagram) === */}
         <div className='flex items-center gap-6'>
           {/* GitHub */}
@@ -50,19 +52,23 @@ const Navbar = () => {
         </div>
 
         {/* === Center Nav Links === */}
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
-          <li><a href="#top">Home</a></li>
-          <li><a href="#top">About Me</a></li>
-          <li><a href="#top">Services</a></li>
-          <li><a href="#top">My Projects</a></li>
-          <li><a href="#top">Contact Me</a></li>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+          <li><a className='font-Ovo' href="#top">Home</a></li>
+          <li><a className='font-Ovo' href="#about">About Me</a></li>
+          <li><a className='font-Outfit' href="#services">Services</a></li>
+          <li><a className='font-Outfit' href="#projects">My Projects</a></li>
+          <li><a className='font-Outfit' href="#contact">Contact Me</a></li>
         </ul>
 
         {/* === Contact Button === */}
-        <div>
+        <div className='flex items-center gap-4'>
+          <button>
+            <Image src={assets.moon_icon} alt=" " className='w-6' />
+          </button>
+
           <a 
             href="#contact" 
-            className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>
+            className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Outfit'>
             Contact 
             <Image 
               src={assets.arrow_icon} 
@@ -70,7 +76,36 @@ const Navbar = () => {
               className='w-3' 
             />
           </a>
+
+          <button className='block md:hidden ml-3'>
+            <Image src={assets.menu_black} alt=" " className='w-6' />
+          </button>
+
         </div>
+
+       {/* === Mobile Menu === */}
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-0 
+        top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+
+          <div className='absolute right-6 top-6'> 
+            <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+          </div>
+          <li className='md:hidden'>
+            <a className='font-Ovo' href="#top">Home</a>
+          </li>
+          <li className='md:hidden'>
+            <a className='font-Ovo' href="#about">About Me</a>
+          </li>
+          <li className='md:hidden'>
+            <a className='font-Outfit' href="#services">Services</a>
+          </li>
+          <li className='md:hidden'>
+            <a className='font-Outfit' href="#projects">My Projects</a>
+          </li>
+          <li className='md:hidden'>
+            <a className='font-Outfit' href="#contact">Contact Me</a>
+          </li>
+        </ul>
       </nav>
     </>
   )
